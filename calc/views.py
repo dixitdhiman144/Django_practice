c669377083cbb121b245a1acbbad52cf6c92ed7b
Dixit Dhiman <dixitdhiman144@gmail.com>
2023-01-07 13:55:54 +0530

    Addition of two Numbers in Django

--- calc/views.py
+++ calc/views.py
@@ -7,7 +7,12 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name':'dixit'})  
 
+def add(request):
 
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request, 'result.html', {'add_result': res})
 
 
 #activate env we use command : workon env_name
