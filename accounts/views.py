007104f11ef121070611122edb5223519aa3dff0
Dixit Dhiman <dixitdhiman144@gmail.com>
2023-01-23 23:24:43 +0530

    Login functionalty part 4

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
-from django.contrib.auth import authenticate
-from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # Create your views here.
 
